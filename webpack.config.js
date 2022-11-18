ffb90dd34e0d0fd02bf87db9c6cfbd905139b50a
Camilo Cunha de Azevedo <camilotk@gmail.com>
2022-11-18 12:24:20 -0300

    🔧 Configurei o WebPack para trabalhar com Shader

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"]
+            },
+	    { 
+                test: /\.(wgsl|glsl|vs|fs)$/,
+                loader: 'ts-shader-loader'
             }
         ]
     }
